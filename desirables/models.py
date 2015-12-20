fe241926b54f543a0b30f6694d4dc7f284d1eb16
Darren Richardson <mnestis@users.noreply.github.com>
2015-12-20 22:04:32 +0000

    Created initial model WishedForItem
    At the moment there's only one wishlist shared by all users.

--- desirables/models.py
+++ desirables/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class WishedForItem(models.Model):
+    url = models.URLField(max_length=1000)
+    date_added = models.DateTimeField(auto_now_add=True)
+    purchased = models.BooleanField(default=False)
+    number_wished_for = models.SmallIntegerField(default=1)
+    thumbnail = models.ImageField(upload_to="thumbnails/", blank=True)
