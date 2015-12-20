5c12be4415ceeceea916e874a597b9713209ef99
Darren Richardson <mnestis@users.noreply.github.com>
2015-12-20 18:23:28 +0000

    Added an initial test, and configuration for Travis CI

--- desirables/tests.py
+++ desirables/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
 # Create your tests here.
+
+class IndexTestCase(TestCase):
+    
+    def test_template(self):
+        client = Client()
+        response = client.get("/")
+        self.assertEqual(response.status_code, 200)
